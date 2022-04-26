4c10d6f11a27280c704b1ea780fe37abac1eca86
David Decottigny <ddecottigny@gmail.com>
2022-04-26 17:34:25 +0200

    Mise en place de l'authentification cryptée d'un utilisateur présent dans la base de données

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -18,4 +18,16 @@ exports.signup = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then()
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
