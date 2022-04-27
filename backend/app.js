7f6dfb96276acd1891e26d35e677e8804d5b7b02
David Decottigny <ddecottigny@gmail.com>
2022-04-27 15:02:46 +0200

    Ajout middleware permettant d'interpréter les requêtes contenant des données de type json

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
