60b26fbc09700c99ff7095aa26fc3a055433117f
David Decottigny <ddecottigny@gmail.com>
2022-04-27 12:24:09 +0200

    Création de l'authentification des utilisateurs par la méthode de  tokens

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
@@ -26,7 +27,15 @@ exports.login = (req, res, next) => {
       }
       bcrypt
         .compare(req.body.password, user.password)
-        .then()
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: "TOKEN",
+          });
+        })
         .catch((error) => res.status(500).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
