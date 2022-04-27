142f02309c35691104dce2db403dfa6a5c6043c6
David Decottigny <ddecottigny@gmail.com>
2022-04-27 14:40:06 +0200

    Création d'un middleware contenant des headers permettant le Cross Origin Resource Sharing entre des serveurs différents

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,19 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 const userRoutes = require("./routes/user");
 
 mongoose
