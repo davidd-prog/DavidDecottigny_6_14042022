bf0be1ea89a8a8e6189a2b5d6c1210d38f095faf
David Decottigny <ddecottigny@gmail.com>
2022-04-29 11:36:16 +0200

    Création d'une sauce réussie mais sans les paramètres obligatoires like, dislike ...

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const sauceSchema = mongoose.Schema({
-  userId: { type: String, required: true },
+  userId: { type: String },
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  usersLiked: { type: Array, required: true },
-  usersDisliked: { type: Array, required: true },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  usersLiked: { type: Array },
+  usersDisliked: { type: Array },
 });
 
 module.exports = mongoose.model("Sauce", sauceSchema);
